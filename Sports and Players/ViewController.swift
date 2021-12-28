f8a3ebd3c2bec6409eb9a918d1f16ac9a1086006
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-12-28 14:26:42 +0300

    Initial Commit

---
+++ Sports and Players/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports and Players
+//
+//  Created by Linah abdulaziz on 24/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
